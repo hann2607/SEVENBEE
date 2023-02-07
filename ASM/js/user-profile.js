7e82eedd6c971291a74b4db7552958b7d8c4fe79
Khanh <hoangkhanh2420@gmail.com>
2023-02-07 14:20:57 +0700

    fix avatar

--- ASM/js/user-profile.js
+++ ASM/js/user-profile.js
@@ -1,22 +1,15 @@
-$(document).ready(function() {
-	
-    var readURL = function(input) {
-        if (input.files && input.files[0]) {
-            var reader = new FileReader();
 
-            reader.onload = function (e) {
-                $('.profile-pic').attr('src', e.target.result);
-            }
-    
-            reader.readAsDataURL(input.files[0]);
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            $('#imagePreview').css('background-image', 'url('+e.target.result +')');
+            $('#imagePreview').hide();
+            $('#imagePreview').fadeIn(650);
         }
+        reader.readAsDataURL(input.files[0]);
     }
-   
-    $(".file-upload").on('change', function(){
-        readURL(this);
-    });
-    
-    $(".upload-button").on('click', function() {
-       $(".file-upload").click();
-    });
+}
+$("#imageUpload").change(function() {
+    readURL(this);
 });
