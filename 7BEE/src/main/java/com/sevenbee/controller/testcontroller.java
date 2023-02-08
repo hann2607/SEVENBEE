902ec971781be214f0112bb7cdf23d42cb8e9e8f
Khanh <hoangkhanh2420@gmail.com>
2023-02-09 00:53:41 +0700

    DAO, Enty dbo.chitiet_sanpham

--- 7BEE/src/main/java/com/sevenbee/controller/testcontroller.java
+++ 7BEE/src/main/java/com/sevenbee/controller/testcontroller.java
@@ -6,17 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.sevenbee.dao.CHITIET_SANPHAMDAO;
 import com.sevenbee.dao.NGUOIDUNGDAO;
+import com.sevenbee.entities.CHITIET_SANPHAM;
 import com.sevenbee.entities.NGUOIDUNG;
 
 @Controller
 public class testcontroller {
 	@Autowired 
-	NGUOIDUNGDAO nguoidungdao;
+	CHITIET_SANPHAMDAO ctspdao;
 	
 	@RequestMapping("/")
 	public String home() {
-		List<NGUOIDUNG> listnd = nguoidungdao.findAll();
+		List<CHITIET_SANPHAM> listnd = ctspdao.findAll();
 		System.out.println(listnd.toString());
 		return "index";
 	}
