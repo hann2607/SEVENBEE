8773d8ef9d36115314c65b66d33ace2554e2ef23
Khanh <hoangkhanh2420@gmail.com>
2023-02-10 23:30:01 +0700

    asd

--- 7BEE/src/main/java/com/sevenbee/controller/detai_ProductController.java
+++ 7BEE/src/main/java/com/sevenbee/controller/detai_ProductController.java
@@ -2,31 +2,19 @@ package com.sevenbee.controller;
 
 import java.io.IOException;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.sevenbee.dao.CHITIET_SANPHAMDAO;
-import com.sevenbee.dao.NGUOIDUNGDAO;
-import com.sevenbee.dao.SANPHAMDAO;
 import com.sevenbee.util.PageInfo;
 import com.sevenbee.util.PageType;
 
 import jakarta.servlet.ServletException;
 
 @Controller
-public class homeController {
-	@Autowired 
-	NGUOIDUNGDAO nguoidungdao;
-	@Autowired 
-	CHITIET_SANPHAMDAO ctspdao;
-	@Autowired 
-	SANPHAMDAO sanphamdao;
-	
-	@SuppressWarnings("unused")
+public class detai_ProductController {
 	@RequestMapping("/")
-	public String home(Model model) throws ServletException, IOException {
+	public String detai_Product(Model model) throws ServletException, IOException {
 //		Optional<SANPHAM> ctsp = sanphamdao.findById("SP005");
 //		System.out.println(ctsp.get().getCt_sanpham().getCTSP_MoTa());
 		
