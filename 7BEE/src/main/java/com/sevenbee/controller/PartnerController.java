7b4d62cd5d172d6f030a5462b5e1158098da3595
Khanh <hoangkhanh2420@gmail.com>
2023-02-20 14:34:37 +0700

    Update PartnerController.java

--- 7BEE/src/main/java/com/sevenbee/controller/PartnerController.java
+++ 7BEE/src/main/java/com/sevenbee/controller/PartnerController.java
@@ -33,7 +33,7 @@ public class PartnerController {
 	public String detai_Product(Model model,@ModelAttribute("product") PRODUCT p) throws ServletException, IOException {
 //		System.out.println(randomService.randomString(5));
 		List<LOAISP> lstLoaiSP = loaispDAO.findAll();
-		System.out.println(lstLoaiSP.size());
+		System.out.println(lstLoaiSP);
 		model.addAttribute("lstLoaiSP", lstLoaiSP);
 		return PageInfo.goSite(model, PageType.SITE_PARTNER);
 	}
