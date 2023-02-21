51852debb8195f9be2f0b89fa426e9dccbf57b59
Khanh <hoangkhanh2420@gmail.com>
2023-02-22 01:33:15 +0700

    up

--- 7BEE/src/main/java/com/sevenbee/controller/PartnerController.java
+++ 7BEE/src/main/java/com/sevenbee/controller/PartnerController.java
@@ -4,7 +4,10 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,14 +15,17 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.sevenbee.dao.CHITIET_SANPHAMDAO;
 import com.sevenbee.dao.LOAISPDAO;
+import com.sevenbee.dao.SANPHAMDAO;
+import com.sevenbee.entity.CHITIET_SANPHAM;
 import com.sevenbee.entity.LOAISP;
 import com.sevenbee.entity.PRODUCT;
+import com.sevenbee.entity.SANPHAM;
 import com.sevenbee.service.CookieService;
 import com.sevenbee.service.RandomService;
 import com.sevenbee.util.PageInfo;
@@ -27,6 +33,7 @@ import com.sevenbee.util.PageType;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 
 @Controller
 public class PartnerController {
@@ -36,6 +43,13 @@ public class PartnerController {
 
 	@Autowired
 	LOAISPDAO loaispDAO;
+	
+	@Autowired
+	SANPHAMDAO spDAO;
+	
+	@Autowired
+	CHITIET_SANPHAMDAO ctspDAO;
+	
 
 	@Autowired
 	RandomService randomService;
@@ -43,49 +57,112 @@ public class PartnerController {
 	@GetMapping("/partner")
 	public String detai_Product(Model model, @ModelAttribute("product") PRODUCT p)
 			throws ServletException, IOException {
-//		System.out.println(randomService.randomString(5));
-//		if (cookieService.getValue("username") == null) {
-//			return "forward:/login";
-//		}
+		if (cookieService.getValue("username") == null) {
+			return "forward:/login";
+		}
 		List<LOAISP> lstLoaiSP = loaispDAO.findAll();
 		model.addAttribute("lstLoaiSP", lstLoaiSP);
 		return PageInfo.goSite(model, PageType.SITE_PARTNER);
 	}
 
 	@RequestMapping("/partner/addproduct")
-	public String add_Product(Model model, @ModelAttribute("product") PRODUCT p, BindingResult result,
+	public String add_Product(Model model,@Valid @ModelAttribute("product") PRODUCT p, BindingResult result,
 			HttpServletRequest request, @RequestParam("filesIMG") MultipartFile[] files)
 			throws ServletException, IOException {
 		if (result.hasErrors()) {
 			// validate form
 			return PageInfo.goSite(model, PageType.SITE_PARTNER);
 		} else {
-//			String CTSP_Table = "";
-//			if (!p.getCTSP_MoTa().isEmpty() && !request.getParameter("content1").isEmpty()) {
-//				CTSP_Table += p.getCTSP_MoTa() + "-*-" + request.getParameter("content1") + "-*-";
-//			}
-//			if (request.getParameter("content2") != null && !request.getParameter("content2").isEmpty()) {
-//				CTSP_Table += request.getParameter("title2") + "-*-" + request.getParameter("content2") + "-*-";
-//			}
-//			if (request.getParameter("content3") != null && !request.getParameter("content3").isEmpty()) {
-//				CTSP_Table += request.getParameter("title3") + "-*-" + request.getParameter("content3") + "-*-";
-//			}
-//			if (request.getParameter("content4") != null && !request.getParameter("content4").isEmpty()) {
-//				CTSP_Table += request.getParameter("title4") + "-*-" + request.getParameter("content4") + "-*-";
-//			}
-			System.out.println(files.length);
+			if (files == null || files.length < 2) {
+		        // Không có ảnh nào được tải lên
+				model.addAttribute("itnhat1hinh", "Vui lòng đăng tải ít nhất 2 ảnh!");
+		    }
 			if (files.length > 10) {
-				// handle error
+				// Báo lỗi nếu có nhiều hơn 10 hình ảnh
+				model.addAttribute("toida10hinh", "Bạn chỉ có thể đăng tải tối đa 10 ảnh!");
 			} else {
+				//Xử lí thông số chi tiết dạng table
+				String CTSP_Table = "";
+				if (!p.getCTSP_MoTa().isEmpty() && !request.getParameter("content1").isEmpty()) {
+					CTSP_Table += p.getCTSP_MoTa() + "-*-" + request.getParameter("content1") + "-*-";
+				}
+				if (request.getParameter("content2") != null && !request.getParameter("content2").isEmpty()) {
+					CTSP_Table += request.getParameter("title2") + "-*-" + request.getParameter("content2") + "-*-";
+				}
+				if (request.getParameter("content3") != null && !request.getParameter("content3").isEmpty()) {
+					CTSP_Table += request.getParameter("title3") + "-*-" + request.getParameter("content3") + "-*-";
+				}
+				if (request.getParameter("content4") != null && !request.getParameter("content4").isEmpty()) {
+					CTSP_Table += request.getParameter("title4") + "-*-" + request.getParameter("content4") + "-*-";
+				}
+				if (request.getParameter("content5") != null && !request.getParameter("content5").isEmpty()) {
+					CTSP_Table += request.getParameter("title5") + "-*-" + request.getParameter("content5") + "-*-";
+				}
+				if (request.getParameter("content6") != null && !request.getParameter("content6").isEmpty()) {
+					CTSP_Table += request.getParameter("title6") + "-*-" + request.getParameter("content6") + "-*-";
+				}
+				if (request.getParameter("content7") != null && !request.getParameter("content7").isEmpty()) {
+					CTSP_Table += request.getParameter("title7") + "-*-" + request.getParameter("content7") + "-*-";
+				}
+				if (request.getParameter("content8") != null && !request.getParameter("content8").isEmpty()) {
+					CTSP_Table += request.getParameter("title8") + "-*-" + request.getParameter("content8") + "-*-";
+				}
+//				System.out.println(CTSP_Table);
 				try {
+					// Lưu hình ảnh về thư mục sản phẩm
 					String uploadDirectory = "src/main/webapp/views/images/product/large-size/";
-					for (MultipartFile file : files) {
-						Path filePath = Paths.get(uploadDirectory, file.getOriginalFilename());
-						Files.write(filePath, file.getBytes());
+					// Mảng hình được tạo sẳn dựa trên số hình ảnh đăng tải
+					String[] lstIMGname = new String[files.length];
+					// Vòng lặp để thêm hình vào folder và thêm tên hình vào mảng
+					for (int i = 0; i < files.length; i++) {
+						// Lấy hình từ mảng input trả về
+			            Path filePath = Paths.get(uploadDirectory, files[i].getOriginalFilename());
+			            // Lưu hình vào thư mục theo đường dẫn
+			            Files.write(filePath, files[i].getBytes());
+			            //Thêm từng tên hình vào mảng
+			            lstIMGname[i] = files[i].getOriginalFilename();
+			        }
+					//Chèn thêm kí hiệu đặc biệt giữa các phần tử trong mảng và đưa về dạng chuỗi
+					 String lstIMGsp = String.join("-*-", lstIMGname);
+//					 System.out.println(joinedlstIMGname);
+					 
+					 //Thêm sản phẩm vào bảng chi tiết
+					 String maCTSP = "CTSP" + cookieService.getValue("username") + randomService.randomString(5) ;
+					 Optional<CHITIET_SANPHAM> ctsp =  ctspDAO.findById(maCTSP);
+					 Optional<SANPHAM> sp =  spDAO.findById(maCTSP.substring(2));
+					  
+					 if (!ctsp.isPresent() || !sp.isPresent()) {
+						 model.addAttribute("error", "Thêm sản phẩm thất bại, vui lòng thử lại! ");
+					}else {
+						//Thêm dữ liệu vào bảng Chi tiết sản phẩm
+						CHITIET_SANPHAM ctspNEW = new CHITIET_SANPHAM();
+						ctspNEW.setCTSP_MA(maCTSP);
+						ctspNEW.setCTSP_MoTa(CTSP_Table);
+						ctspNEW.setKich_Thuoc(p.getCTSP_Mau());
+						ctspNEW.setCTSP_Mau(p.getCTSP_Mau());
+						ctspNEW.setCTSP_ThongTinThem(p.getCTSP_ThongTinThem());
+						ctspDAO.save(ctspNEW);
+						
+						//Thêm dữ liệu vào bảng Sản phẩm
+						SANPHAM spNEW = new SANPHAM();
+						spNEW.setSP_MA(maCTSP.substring(2));
+						spNEW.setShop(null);/////////
+						spNEW.setLoaisp(loaispDAO.findById(p.getLoaiSP_MA()).get());
+						Date currentDate =  new Date();
+						spNEW.setSP_Ngaydang(currentDate);
+						spNEW.setSP_TenSP(p.getSP_TenSP());
+						spNEW.setSP_HinhAnh(lstIMGsp);
+						spNEW.setSP_Gia(p.getSP_Gia());
+						spNEW.setSP_SoLuong(p.getSP_SoLuong());
+						spNEW.setCt_sanpham(ctspDAO.findById(maCTSP).get());
+						spDAO.save(spNEW);
 					}
 				} catch (IOException e) {
+					model.addAttribute("error", "Error: " + e.getMessage());
 				}
 			}
+			
+			
 		}
 		return PageInfo.goSite(model, PageType.SITE_PARTNER);
 	}
--- 7BEE/src/main/webapp/views/jsp/Partner/partner_form.jsp
+++ 7BEE/src/main/webapp/views/jsp/Partner/partner_form.jsp
@@ -48,7 +48,8 @@
 				aria-controls="nav-home" aria-selected="true">Chi tiết</button>
 			<button class="nav-link" id="nav-profile-tab" data-toggle="tab"
 				data-target="#nav-profile" type="button" role="tab"
-				aria-controls="nav-profile" aria-selected="false">Danh sách sản phẩm</button>
+				aria-controls="nav-profile" aria-selected="false">Danh sách
+				sản phẩm</button>
 			<button class="nav-link" id="nav-contact-tab" data-toggle="tab"
 				data-target="#nav-contact" type="button" role="tab"
 				aria-controls="nav-contact" aria-selected="false">Đơn hàng</button>
@@ -59,6 +60,12 @@
 			aria-labelledby="nav-home-tab">
 			<h3 class="d-flex justify-content-center m-4 font-weight-bold">THÔNG
 				TIN SẢN PHẨM</h3>
+			<c:if test="${not empty message }">
+				<div class="alert alert-success" id="success">${message }</div>
+			</c:if>
+			<c:if test="${not empty error }">
+				<div class="alert alert-danger" id="error">${error }</div>
+			</c:if>
 			<form:form modelAttribute="product" enctype="multipart/form-data">
 				<div class="row">
 					<div class="col-6">
@@ -117,12 +124,22 @@
 						</div>
 					</div>
 					<div class="col-6">
-						<div class="row h-50 d-flex flex-column">
-							<h5 class="text-center mb-0">Ảnh sản phẩm</h5>
+						<div class="row d-flex flex-column">
+							<h5 class="text-center mb-0">
+								Ảnh sản phẩm
+								<p class="text-danger font-weight-bold">${toida10hinh}${itnhat1hinh}</p>
+							</h5>
 							<div class="d-flex flex-column">
 								<div id="selectedImages" class="carousel slide"
 									data-ride="carousel">
-									<div class="carousel-inner"></div>
+									<div class="carousel-inner">
+										<c:if test="${empty param.filesIMG}">
+											<div style="height: 17em;" class="carousel-item active">
+												<img src="/views/images/product/large-size/product.png"
+													class="d-block w-50 h-100 mx-auto img-thumbnail img-fluid">
+											</div>
+										</c:if>
+									</div>
 									<a class="carousel-control-prev" href="#selectedImages"
 										role="button" data-slide="prev"> <span
 										class="fa fa-chevron-left fa-lg" style="color: black;"></span>
@@ -135,54 +152,80 @@
 								</div>
 								<div class="d-lg-flex justify-content-center">
 									<div class="file-input btn">
-										<span><i class="fa-regular fa-image"></i> Chọn ảnh</span> <input accept=".jpg,.png,.gif"
-											type="file" name="filesIMG" max="10" multiple onchange="displaySelectedImages(this)">
+										<span><i class="fa-regular fa-image"></i> Chọn ảnh</span> <input
+											accept=".jpg,.png,.gif" type="file" name="filesIMG" max="10"
+											multiple onchange="displaySelectedImages(this)">
 									</div>
 								</div>
 							</div>
 						</div>
-						<div class="row h-50">
+						<div class="row mt-25">
 							<h5 class="text-center w-100 mb-0">
 								Thông số chi tiết
 								<form:errors path="CTSP_MoTa"
 									class="text-danger font-weight-bold" />
 							</h5>
-
-							<table id="dataTableThemSP" class="table">
-								<thead class="thead-light">
-									<tr>
-										<th>Tiêu đề</th>
-										<th>Nội dung</th>
-									</tr>
-								</thead>
-								<tbody>
-									<tr>
-										<td><form:input path="CTSP_MoTa" type="text"
-												class="form-control" placeholder="Nhập tiêu đề 1" /></td>
-										<td><input type="text" class="form-control"
-											name="content1" placeholder="Nhập nội dung 1" /></td>
-									</tr>
-									<tr>
-										<td><input type="text" class="form-control" name="title2"
-											placeholder="Nhập tiêu đề 2"></td>
-										<td><input type="text" class="form-control" name="content2"
-											placeholder="Nhập nội dung 2"></td>
-									</tr>
-									<tr>
-										<td><input type="text" class="form-control" name="title3"
-											placeholder="Nhập tiêu đề 3"></td>
-										<td><input type="text" class="form-control" name="content3"
-											placeholder="Nhập nội dung 3"></td>
-									</tr>
-									<tr>
-										<td><input type="text" class="form-control" name="title4"
-											placeholder="Nhập tiêu đề 4"></td>
-										<td><input type="text" class="form-control" name="content4"
-											placeholder="Nhập nội dung 4"></td>
-									</tr>
-									<!-- Các hàng tiếp theo tương tự -->
-								</tbody>
-							</table>
+							<div class="table-responsive" style="height: 16em">
+								<table id="dataTableThemSP" class="table border">
+									<thead class="thead-light">
+										<tr>
+											<th>Tiêu đề</th>
+											<th>Nội dung</th>
+										</tr>
+									</thead>
+									<tbody>
+										<tr>
+											<td><form:input path="CTSP_MoTa" type="text"
+													class="form-control" placeholder="Nhập tiêu đề 1" /></td>
+											<td><input type="text" class="form-control"
+												name="content1" placeholder="Nhập nội dung 1" /></td>
+										</tr>
+										<tr>
+											<td><input type="text" class="form-control"
+												name="title2" placeholder="Nhập tiêu đề 2"></td>
+											<td><input type="text" class="form-control"
+												name="content2" placeholder="Nhập nội dung 2"></td>
+										</tr>
+										<tr>
+											<td><input type="text" class="form-control"
+												name="title3" placeholder="Nhập tiêu đề 3"></td>
+											<td><input type="text" class="form-control"
+												name="content3" placeholder="Nhập nội dung 3"></td>
+										</tr>
+										<tr>
+											<td><input type="text" class="form-control"
+												name="title4" placeholder="Nhập tiêu đề 4"></td>
+											<td><input type="text" class="form-control"
+												name="content4" placeholder="Nhập nội dung 4"></td>
+										</tr>
+										<tr>
+											<td><input type="text" class="form-control"
+												name="title5" placeholder="Nhập tiêu đề 5"></td>
+											<td><input type="text" class="form-control"
+												name="content5" placeholder="Nhập nội dung 5"></td>
+										</tr>
+										<tr>
+											<td><input type="text" class="form-control"
+												name="title6" placeholder="Nhập tiêu đề 6"></td>
+											<td><input type="text" class="form-control"
+												name="content6" placeholder="Nhập nội dung 6"></td>
+										</tr>
+										<tr>
+											<td><input type="text" class="form-control"
+												name="title7" placeholder="Nhập tiêu đề 7"></td>
+											<td><input type="text" class="form-control"
+												name="content7" placeholder="Nhập nội dung 7"></td>
+										</tr>
+										<tr>
+											<td><input type="text" class="form-control"
+												name="title8" placeholder="Nhập tiêu đề 8"></td>
+											<td><input type="text" class="form-control"
+												name="content8" placeholder="Nhập nội dung 8"></td>
+										</tr>
+										<!-- Các hàng tiếp theo tương tự -->
+									</tbody>
+								</table>
+							</div>
 						</div>
 					</div>
 				</div>
